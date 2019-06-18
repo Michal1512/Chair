d49fa3e6ea8a5a07a33803b4ad352b82a1406f2a
Kacper2311 <47105097+Kacper2311@users.noreply.github.com>
2019-06-18 12:37:25 +0200

    Chair

---
+++ webpack.config.js
@@ -0,0 +1,99 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+var path = require('path');
+var webpack = require('webpack');
+var WebpackBuildNotifierPlugin = require('webpack-build-notifier');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const production = process.argv.indexOf('-p') !== -1;
+
+
+var config = {
+    entry: [
+        './resources/js/index.js',
+        './resources/css/scss/main.scss'
+    ],
+    output: {
+        filename: 'js/[name].bundle.js',
+        path: path.resolve(__dirname, './bundles'),
+        publicPath: "/"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(css|sass|scss)$/,
+                exclude: /(node_modules)/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    //resolve-url-loader may be chained before scss-loader if necessary
+                    use: ['file-loader?name=css/[name].css', 'extract-loader', 'css-loader', 'autoprefixer-loader', 'sass-loader']
+                })
+            }
+//            , {
+//                test: /\.js$/,
+//                use: 'imports-loader?define=>false',
+//            }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin({
+            filename: 'css/tmp.css'
+                    // allChunks: true
+        }),
+//        new webpack.DefinePlugin({
+//            'process.env': {
+//                NODE_ENV: JSON.stringify('production')
+//            }
+//        })
+//        new webpack.optimize.CommonsChunkPlugin({
+//            name: 'commons',
+//            filename: 'js/commons.js',
+//            minChunks: 2
+//        })
+    ],
+    resolve: {
+        //alias: {'picker': 'pickadate/lib/picker'}
+        alias: {
+            'vue$': 'vue/dist/vue.esm.js'
+        }
+    }
+};
+
+if (production) {
+    config.entry.babelpolyfill = 'babel-polyfill';
+    config.module.rules.push({
+        test: /\.js$/,
+        use: [{
+            loader: 'babel-loader',
+            options: {
+                presets: [['es2015', 'stage-2']],
+                plugins: ['syntax-dynamic-import']
+            }
+        }]
+    });
+    config.plugins.push(new webpack.optimize.UglifyJsPlugin({
+        compress: {
+            warnings: false,
+            // Drop console statements
+            drop_console: true
+        },
+        output: {
+            comments: false,
+            "ascii_only": true
+        }
+    }));
+
+
+} else {
+    config.plugins.push(new WebpackBuildNotifierPlugin());
+    config.devtool = 'source-map'; //'inline-eval-cheap-source-map';
+}
+
+
+module.exports = config;
+
+
